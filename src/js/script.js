ef107b638e0f7fe613b147d86674921d917c037a
easygoing2 <topgun8127@naver.com>
2023-05-29 17:24:21 +0900

    innerhtml작업중
    상품과 유저 작업 중

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,48 @@
 const products = [
-  { name: "코카콜라", price: 1200 },
-  { name: "펩시", price: 1100 },
-  { name: "사이다", price: 1500 },
-  { name: "환타", price: 900 },
-  { name: "오렌지쥬스", price: 2000 }
+  { name: "아이스 아메리카노", price: 1500, url: "./src/img/imgCoffee01.png" },
+  { name: "뜨뜻한 아메리카노", price: 1500, url: "./src/img/imgCoffee02.png"  },
+  { name: "카페라떼", price: 2000, url: "./src/img/imgCoffee03.png"  },
+  { name: "카라멜 마끼아또", price: 2000, url: "./src/img/imgCoffee04.png"  },
+  { name: "카페모카", price: 2500, url: "./src/img/imgCoffee05.png"  },
+  { name: "돌체라떼", price: 2200, url: "./src/img/imgCoffee06.png"  }
 ];
 
-var productsDiv = document.getElementById("products");
-for (var i = 0; i < products.length; i++) {
-  var product = products[i];
+const user = [
+  { name: "제임스", money: 15000 },
+  { name: "벨라", money: 10500 },
+  { name: "제이콥", money: 20000 },
+  { name: "컬렌", money: 20000 },
+  { name: "로잘리", money: 25000 },
+  { name: "에드워드", money: 22000 }
+];
+
+// 상품 뿌려줌
+let productWraps = document.querySelector('.productWrap');
+productWraps.innerHTML = "";
+
+for (let i = 0; i < products.length; i++) {
+  let product = products[i];
   console.log("제품명 : ", product.name);
-  var productDiv = document.createElement("div");
-  productDiv.className = "product"; 
+  console.log("제품가격 : ", product.price);
+  console.log("제품이미지 : ", product.url);
+  productWraps.innerHTML +=
+    `<div class="product btn--product">
+    <img src="` + product.url + `" alt="아메리카노">
+    <p class="productName">` + product.name + `</p>
+    <div class="productPrice">` + product.price + `원</div>
+    </div>`  
+};
 
-  var productButton = document.createElement("button");
-  productButton.className = "product-button";
-  productButton.innerHTML = product.name + "<br>" + product.price + "원";
-  productDiv.appendChild(productButton);
-  //productsDiv.appendChild(productDiv);
-}
+// 유저이름 & 유저금액 보여줌
+let userWraps = document.querySelector('.user');
+userWraps.innerHTML = "";
+let moneyWraps = document.querySelector('.money');
+moneyWraps.innerHTML = "";
 
-var priceButtonsDiv = document.getElementById("price-buttons");
-var priceButtons = [1000, 500, 100];
-for (var i = 0; i < priceButtons.length; i++) {
-  var priceButton = document.createElement("button");
-  priceButton.className = "price-button";
-  priceButton.innerHTML = priceButtons[i] + "&#65533;&#65533;";
-  //priceButtonsDiv.appendChild(priceButton);
-}
+for (let i = 0; i < user.length; i++) {
+  let users = user[0];
+  console.log("이름 :", users.name);
+  console.log("금액", users.name);
+  userWraps.innerHTML = users.name;
+  moneyWraps.innerHTML = users.money;
+};
