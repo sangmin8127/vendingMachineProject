2faffb2c9271ad662b8a13acc1d08fa9978cf595
sangmin <sml@onns.co.kr>
2023-06-03 17:44:53 +0900

    레이아웃 및 시나리오 변경

--- src/js/script.js
+++ src/js/script.js
@@ -7,14 +7,6 @@ const products = [
   { name: "돌체라떼", price: 2200, url: "./src/img/imgCoffee06.png"  }
 ];
 
-const statusText = [
-  { txt: "Ice 아메리카노", price: 1500, url: "./src/img/imgCoffee01.png" },
-  { txt: "Hot 아메리카노", price: 1500, url: "./src/img/imgCoffee02.png"  },
-  { txt: "카페라떼", price: 2000, url: "./src/img/imgCoffee03.png"  },
-  { txt: "카라멜 마끼아또", price: 2000, url: "./src/img/imgCoffee04.png"  },
-  { txt: "카페모카", price: 2500, url: "./src/img/imgCoffee05.png"  },
-  { txt: "돌체라떼", price: 2200, url: "./src/img/imgCoffee06.png"  }
-];
 
 // 상품 뿌려줌
 let productWraps = document.querySelector('.productWrap');
@@ -26,56 +18,60 @@ for (let i = 0; i < products.length; i++) {
   console.log("제품가격 : ", product.price);
   console.log("제품이미지 : ", product.url);
   productWraps.innerHTML +=
-  `<div class="product btn--product" onclick="productBtn(this)">
+  `<div class="product btn--product disabledButton" onclick="choiceProductBtn(this)">
   <img class="productImg" src="` + product.url + `" alt="상품이미지">
   <p class="productName">` + product.name + `</p>
   <div class="productPrice">` + product.price + `</div>
   </div>`  
 };
 
+// 상태메세지 뿌려줌
 
-// 상품 클릭시 아래 선택상품에 제품명, 가격 노출.
-// 이미 뿌려진 상품들이 클릭되면 onclick="productBtn(this)" 함수 실행됨.
-productChioceName.innerHTML = "";
-productChiocePrice.innerHTML = "";
 
-function productBtn(element) {
-  if (productInsertPrice.innerHTML == "0") {
-    infoTxt.innerHTML = "금액을 먼저 투입해주세요."
-  } else {
-    proImg = element.querySelector('.productImg').src;
-    proName = element.querySelector('.productName').innerHTML;
-    proPrice = element.querySelector('.productPrice').innerHTML;
-    console.log("this :", proImg, proName, proPrice);
-    productChioceImg.src = proImg;
-    productChioceName.innerHTML = proName;
-    productChiocePrice.innerHTML = proPrice;
-    console.log(productInsertPrice.innerHTML);
+// 상품 클릭 Event. 
+function choiceProductBtn() {
+  if (productInsertPrice.innerHTML == "0") {  // 투입금액이 0원이면 안내 메세지 노출
+    
+  } 
+  else if (productInsertPrice.innerHTML !== "0") {  // 투입금액이 0원이면 안내 메세지 노출
+
+  } 
+  // 투입 금액이 상품가격과 일치하면 구매하기 버튼 활성화 & 메세지 수정.
+  else if (productInsertPrice == productChiocePrice) {
+    alert("투입금액과 상품 가격이 일치합니다.");
   }
 };
 
-// 투입금액 버튼 클릭 시 금액합산해서 보여줌.
+// 투입금액 영역 초기화 
 let sum = 0;
-let proPrice = productChiocePrice.innerHTML;
-let proName = productChioceName.innerHTML;
 
+// 투입 금액 버튼 클릭 
+function insertMoney(element) {
+  if (element) {
+    calcMoney(element)
+    console.log("금액이" + element.value + "투입됩니다.");
+    console.log("투입된 총 금액이" + sum + "원 입니다.");
+  }
+  productInsertPrice.innerHTML = sum; // 투입금액 영역에 텍스트로 뿌려줌
+  let products = document.querySelectorAll('.product'); // 모든 상품 변수에 담아줌.
+  for (let i = 0; i < products.length; i++) {
+    products[i].classList.remove('disabledButton');  // disabledButton 클래스 삭제하여 제품 활성화
+  }
+};
 
-function calc(button) {
-  if(button) {
-    sum+= parseInt(button.value);
-  }
-	else {
-    sum-= button.value;
-  }
-  productInsertPrice.innerHTML = sum;
-  infoTxt.innerHTML = "상품을 선택해 주세요."
+// 투입금액 더하기 연산
+function calcMoney(element) {
+  sum+= parseInt(element.value);
 }
+
+
 //parseInt를 안하면 값이 더하기가 안되고 옆에 붙여넣기가 됨
 //this는 이 버튼을 클릭했을 때 참조값
 //function()에 들어갈 변수이름은 아무거나
 
-// 초기화 버튼
-function reset() {
+// 투입 금액 초기화 버튼
+function resetMoney() {
   sum = 0;
   productInsertPrice.innerHTML = sum;
+
 }
